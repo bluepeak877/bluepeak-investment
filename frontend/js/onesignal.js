3a387bb03d0e0aac5cdecc138d4bdc0adcb54ed1
bluepeak877 <bluepeak.ltd.co@gmail.com>
2026-06-29 08:48:37 +0100

    Update onesignal.js

--- frontend/js/onesignal.js
+++ frontend/js/onesignal.js
@@ -1,1 +1,12 @@
-alert("ONESIGNAL FILE IS EXECUTING");
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+
+OneSignalDeferred.push(async function (OneSignal) {
+
+    await OneSignal.init({
+        appId: "b0508782-f7ca-4181-b98e-59295e34c725",
+    });
+
+    // Show native browser permission prompt
+    await OneSignal.Notifications.requestPermission();
+
+});

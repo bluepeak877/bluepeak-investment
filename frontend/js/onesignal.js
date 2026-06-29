70765593e7a612d0392a10f8f7238a7ee580c745
bluepeak877 <bluepeak.ltd.co@gmail.com>
2026-06-29 08:21:18 +0100

    Update onesignal.js

--- frontend/js/onesignal.js
+++ frontend/js/onesignal.js
@@ -1,17 +1,19 @@
-// ===============================
-// OneSignal Initialization
-// ===============================
+console.log("OneSignal JS Loaded");
+
 window.OneSignalDeferred = window.OneSignalDeferred || [];
 
-OneSignalDeferred.push(async function (OneSignal) {
+window.OneSignalDeferred.push(async function (OneSignal) {
 
-  await OneSignal.init({
-    appId: "b0508782-f7ca-4181-b98e-59295e34c725",
-  });
+    console.log("OneSignal SDK Ready");
 
-  const permission =
+    await OneSignal.init({
+        appId: "b0508782-f7ca-4181-b98e-59295e34c725"
+    });
+
+    console.log("Initialized");
+
     await OneSignal.Notifications.requestPermission();
 
-  console.log("Notification Permission:", permission);
+    console.log("Permission Requested");
 
 });
